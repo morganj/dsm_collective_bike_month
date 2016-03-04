3250e74ab963fe34b0683f2c86995a0454ae2487
Kim Spasaro <spasarok@grinnell.edu>
2016-03-03 22:38:12 -0600

    added sponsor content type

--- functions.php
+++ functions.php
@@ -1,4 +1,22 @@
 <?php 
 
 
-// Event content type
+// Event content type will be created by plugin
+
+// Create sponsored content
+function create_bikes_sponsors() {
+    $labels = array(
+        'name' => __( 'Sponsors' ),
+        'singular_name' => __( 'Sponsor' ),
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => false,
+        'rewrite' => array('slug' => 'sponsor')
+    );
+
+    register_post_type( 'bikes_sponsor', $args);
+}
+add_action( 'init', 'create_bikes_sponsors' );
