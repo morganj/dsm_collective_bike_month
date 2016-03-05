f0b22eca7a8bbd6cae058740314c4a948297888c
crolek <c.rolek@gmail.com>
2016-03-05 14:52:56 -0600

    adding tier 1 sponors

---
+++ wp-content/themes/bikes/includes/event-redeemer/event-redeemer-template.php
@@ -0,0 +1,8 @@
+<div id="event-redeemer" class="<?php echo bikes_event_classes(); ?>">
+    <div class="event-not-redeemed">
+        <input id="event-code" type="text">
+        <button id="event-code-submit">Click Me!</button>
+        <div class="event-code-error"></div>
+    </div>
+    <div class="event-redeemed">Event redeemed</div>
+</div>
